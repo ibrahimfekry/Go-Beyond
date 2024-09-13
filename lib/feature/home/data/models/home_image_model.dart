0e312b9f6bbccb4228440c6cce6a42622238e05c
DESKTOP-IA0DCP6\Al-Ahram <ibrahimfekry159@gmail.cmo>
2024-09-13 16:18:13 +0200

    no message

---
+++ lib/feature/home/data/models/home_image_model.dart
@@ -0,0 +1,13 @@
+part of 'home_page';
+
+class HomeImageModel{
+  HomeImageModel(this.homeImage,this.siteName,this.siteRate,this.siteLocation,this.sitePrice,this.siteDate);
+  String? homeImage;
+  String? siteName;
+  String? siteRate;
+  String? siteLocation;
+  String? sitePrice;
+  String? siteDate;
+
+
+}
